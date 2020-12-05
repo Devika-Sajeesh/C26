8105efa8c59758c202a1297e86939e2b9f97888c
Devika Sajeesh <devikasajeesh26@gmail.com>
2020-12-05 15:30:35 +0530

    added bird1

--- sketch.js
+++ sketch.js
@@ -18,6 +18,8 @@ function setup(){
 
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
+    
+    bird1 = new Bird(400,200);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -58,6 +60,8 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+
+    bird1.display();
 
     bird.display();
     platform.display();
